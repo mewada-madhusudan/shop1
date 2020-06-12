19400a146d93a4ea8dde4ebd954f3074c9c358f9
mewada-madhusudan <madhusudan.145@gmail.com>
2020-06-13 01:18:42 +0530

    Update public_one_col.php

--- application/modules/template/views/public_one_col.php
+++ application/modules/template/views/public_one_col.php
@@ -4,19 +4,19 @@
     <title>David Connelly</title>
 <meta name="description" content="Website maintenance and management service based in the UK offering web updating service and website updating software.">
 <meta name="keywords" content="website maintenance, web management, website management, web site mangement, website updating, web updaters, update website, updating service, UK, website updates, website updating service"><meta http-equiv="content-type" content="text/html;charset=utf-8" />
-<link rel="stylesheet" href="<?php echo base_url(); ?>css/stylesheet.css" type="text/css" />    
+<link rel="stylesheet" href="https://medi-shop.herokuapp.com/css/stylesheet.css" type="text/css" />    
         
         
     </head>
     <body>
         
         <div id="masterhead">
-            <div id="header"><div id="davidconnelly"><a href="<?php echo base_url(); ?>">
-                    <img src="<?php echo base_url(); ?>images/davidconnelly.png" border="0"></a></div>
+            <div id="header"><div id="davidconnelly"><a href="https://medi-shop.herokuapp.com/">
+                    <img src="https://medi-shop.herokuapp.com/images/davidconnelly.png" border="0"></a></div>
             
             
                 <div id="webdeveloper">
-                    <img src="<?php echo base_url(); ?>images/webdeveloper.png">
+                    <img src="https://medi-shop.herokuapp.com/images/webdeveloper.png">
                 </div>
             
             
@@ -24,14 +24,14 @@
             <div id="topnav">
                 
                 <ul>
-                    <li><img src="<?php echo base_url(); ?>images/homemenu.jpg" class="homebtn"></a>
-                    <li><a href="<?php echo base_url(); ?>">Some Link</a></li>
-                    <li><a href="<?php echo base_url(); ?>">Some Link</a></li>
-                    <li><a href="<?php echo base_url(); ?>">Some Link</a></li>
-                    <li><a href="<?php echo base_url(); ?>">Some Link</a></li>
-                    <li><a href="<?php echo base_url(); ?>">Some Link</a></li>
-                    <li><a href="<?php echo base_url(); ?>">Some Link</a></li>
-                    <li><a href="<?php echo base_url(); ?>">Some Link</a></li>
+                    <li><img src="https://medi-shop.herokuapp.com/images/homemenu.jpg" class="homebtn"></a>
+                    <li><a href="https://medi-shop.herokuapp.com/">Some Link</a></li>
+                    <li><a href="https://medi-shop.herokuapp.com/">Some Link</a></li>
+                    <li><a href="https://medi-shop.herokuapp.com/">Some Link</a></li>
+                    <li><a href="https://medi-shop.herokuapp.com/">Some Link</a></li>
+                    <li><a href="https://medi-shop.herokuapp.com/">Some Link</a></li>
+                    <li><a href="https://medi-shop.herokuapp.com/">Some Link</a></li>
+                    <li><a href="https://medi-shop.herokuapp.com/">Some Link</a></li>
                 </ul>
                 
             </div>
